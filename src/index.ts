992a997a68a1586dd7c220094eb2e03e9131cdbf
BakiAbdullah <shatil605@gmail.com>
2025-05-08 21:20:35 +0600

    problem 4 solved

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,57 @@
 function formatString(input: string, toUpper?: boolean) {
   if (toUpper !== false) {
-  return input.toUpperCase()
+    return input.toUpperCase();
   } else {
-  return input.toLowerCase()
+    return input.toLowerCase();
+  }
+}
+//------->
+
+interface Item {
+  title: string;
+  rating: number;
+}
+
+function filterByRating(items: Item[]): Item[] {
+  return items.filter((item) => item.rating >= 4);
+}
+
+const books: Item[] = [
+  { title: "The Alchemist", rating: 4.5 },
+  { title: "1984", rating: 3.8 },
+  { title: "The Great Gatsby", rating: 5.0 },
+];
+//------->
+
+function concatenateArrays<T>(...arrays: T[]): T[] {
+  return arrays.flat() as T[];
+}
+//------->
+
+class Vehicle {
+  private make: string;
+  private year: number;
+
+  constructor(make: string, year: number) {
+    this.make = make;
+    this.year = year;
+  }
+  getInfo() {
+    return console.log(`Make: ${this.make}, Year: ${this.year} `);
   }
 }
 
-// console.log( formatString('baki'))
-// console.log( formatString('baki',true))
-// console.log( formatString('BAKI', false))
+class Car extends Vehicle {
+  private model: string;
+
+  constructor(model: string, year: number, make: string) {
+    super(make, year);
+    this.model = model;
+  }
+  getModel() {
+    return console.log(`Model: ${this.model}`);
+  }
+}
+const myCar = new Car("Mustang Gt", 2010, "Ford");
+// -------->
+
